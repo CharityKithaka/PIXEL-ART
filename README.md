c48f4a208defddcfb496c2c746180592559b48fa
Charity <shikez17@gmail.com>
2018-07-01 23:56:34 +0300

    Modificaton of the README.md Modification of the index.html Modification of the scripts.js Modification of teh styles.css

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # PIXEL-ART
+
+
+#### An art of pixel in JavaScript, 30/06/2018
+
+#### By **Charity Kithaka**
+
+## Description
+The pixel-art is written with JavaScript to practice JavaScript and JQuery.
+
+## Setup/Installation Requirements
+You can clone this repository by clicking the clone and download button and copying the link. 
+
+These are the terminal commands after cloning the project
+```
+git clone https://github.com/CharityKithaka/PIXEL-ART
+
+cd PIXEL-ART
+```
+
+## Known Bugs
+No known bugs
+
+## Technologies Used
+* HTML
+* CSS
+* JavaScript
+* JQuery
+
+## Support and contact details
+Email : shikez17@gmail.com
+
+### License
+*MIT*
+Copyright (c) 2018 **Charity Kithaka**
--- scripts.js
+++ scripts.js
@@ -1,31 +1,67 @@
-// front end logic
+
+// / front end logic
 let width = 0;
 let height = 0;
 
 // let cellRow = "";
 // canvas to update
 const canvas = $('#pixelCanvas');
-// canvas.height() = 10;
-// alert(canvas.height());
-$(".btn-primary").on('click', () => {
 
-  height = Number($('#inputHeight').val());
-	
-	width = Number($('#inputWidth').val());
-	/*
-	*  Button trigger should do the following
-	 - Get the submitted values: height & width
-	
-	 - Use those values to update the canvas: set Height & set Width
-  */
-  
+//this will store the color that is currently on the ColorPicker
+let color = "#000000";
 
-	if(height > 0 && width > 0) {
-    // update canvas
-    canvas.height = height;
-    canvas.width = width;
-    console.log(canvas);
-	} else {
-    console.log('Height & width should be greater than zero');
+
+$("#updateCanvas").on('click', function () {
+
+  //first clear the canvas if it is not empty
+  canvas.empty();
+
+  //get height and convert it to an integer
+  preventDefault();
+  height = parseInt($('#inputHeight').val());
+
+  //get width and convert it to an integer
+  width = parseInt($('#inputWidth').val());
+
+  console.log("Width = " + width + " , height = " + height);
+
+  //start creating the canvas
+  for (var i = 0; i < height; i++) {
+
+    for (var j = 0; j < width; j++) {
+
+      //create CSS attributes that will be appended to a cell on the canvas
+      var cssObj = {
+        'background-color': color,
+        'width': "25px",
+        'height': '25px',
+        'float': 'left',
+        'border': "2px solid #c0c0c0"
+      };
+
+      canvas.append(
+        //attach ID to the cell
+        $("<div></div>", { id: 'id' + i + '_' + j })
+
+          //attach CSS attributes to the cell
+          .css(cssObj)
+
+          // We want if you click on a cell, it updates the color to the one that is on the color picker.		
+          .on('click', function () {
+            console.log("changing to " + color);
+            this.style.backgroundColor = color;
+          }));
+    }
+    canvas.append($("<br/><br/>"));
+
   }
+
+});
+
+
+//set onClick listener to the color picker
+//if you select a new color, it saves the new color automatically.
+$('#colorPicker').on('change', function () {
+  console.log($('#colorPicker').val());
+  color = $('#colorPicker').val();
 });
